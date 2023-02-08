ac2eaac13c409d6c6d279a99e4b6e216bf74b53c
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-02-08 09:25:24 -0500

    Update towerDefenseTokens.js

--- blooket/towerDefenseTokens.js
+++ blooket/towerDefenseTokens.js
@@ -1,1 +1,14 @@
-
+javascript:
+(async () => {
+            function reactHandler() {
+                return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+            };
+        
+            if (window.location.pathname != '/defense') {
+                alert('This cheat only works in the tower defense mode')
+            } else {
+                let tokens = Number(parseFloat(prompt('How much $ do you want?')));
+                reactHandler().stateNode.setState({ tokens });
+                alert("aight bet");
+            };
+})();javascript:void 0;
