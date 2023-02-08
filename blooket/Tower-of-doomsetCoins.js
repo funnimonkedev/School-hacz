67733349c56ffc785faf779f1228c20b386f03d3
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-02-08 10:43:09 -0500

    Update Tower-of-doomsetCoins.js

--- blooket/Tower-of-doomsetCoins.js
+++ blooket/Tower-of-doomsetCoins.js
@@ -9,13 +9,12 @@ javascript:(async () => {
                 return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
             };
         
-            if (window.location.pathname == '/tower/battle' || window.location.pathname == '/tower/start') {
+            if (window.location.pathname == '/tower/battle' || window.location.pathname == '/tower/start' || window.location.pathname == '/tower/shop') {
                 try {
                     reactHandler().stateNode.props.setTowerCoins(Number(parseFloat(prompt('How many coins do you want?'))));
-        
-                    alert('Coins added!')
+                    alert('weener added! ;)')
                 } catch { }
             } else {
-                alert('You must be in a tower of doom game!');
+                doucmne.write("BE IN TOWER OF DOOM NOW");
             };
 })();javascript:void 0;
