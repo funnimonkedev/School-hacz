e80e8ff2701f2a1ae5d902466291364736e0a13c
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-02-08 10:15:24 -0500

    Create FishingFrenzyWeight.js

---
+++ blooket/FishingFrenzyWeight.js
@@ -0,0 +1,20 @@
+javascript:(async () => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.alert = n.contentWindow.alert.bind(window);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.confirm = n.contentWindow.confirm.bind(window);
+    n.remove();
+            function reactHandler() {
+                return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
+            };
+        
+            if (window.location.pathname != '/play/fishing') {
+                document.write("WRONG SITE! =)");
+            } else {
+                let weight = Number(parseFloat(prompt('How much weight do you want?')));
+                reactHandler().stateNode.setState({ weight2: weight, weight });
+        
+                alert('FISH FATS SET!');
+            };
+})();javascript:void 0;
