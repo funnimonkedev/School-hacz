bfc334075e78a7f44be48a325a1bf5ce6bb84760
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-02-08 09:59:30 -0500

    Create SoundMagic.js

---
+++ Bookmarklet/SoundMagic.js
@@ -0,0 +1,1 @@
+javascript:var audio1 = new Audio("https://www.fesliyanstudios.com/play-mp3/3538"); window.addEventListener("keyup", (e)=> {audio1.play();});javascript:void 0;/*plays a very small fard noise*/
