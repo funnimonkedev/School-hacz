ed382fe4119505fdd365639ce3712a5d0792726e
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-02-20 16:12:59 -0500

    Create invertPage(byKrazete).js

---
+++ Bookmarklet/invertPage(byKrazete).js
@@ -0,0 +1,1 @@
+javascript:if (typeof inverthtml == "undefined") {var inverthtml = document.createElement("style");inverthtml.innerHTML = "html{background:black;filter:invert(100%)}";document.body.appendChild(inverthtml);}else {inverthtml.remove();inverthtml = undefined;}javascript:void 0;
